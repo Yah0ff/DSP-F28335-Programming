eafe8434735868711087d40366d035ae6383dcf3
Yah0ff <gagy020416@gs.utm.mx>
2026-06-10 15:27:31 -0600

    DSP-F28335-2

---
+++ 1 . HelloWorld/Debug/Plantilla.d
@@ -0,0 +1,84 @@
+# FIXED
+
+Plantilla.obj: ../Plantilla.c
+Plantilla.obj: C:/ti/c2000/C2000Ware_6_00_00_00/device_support/f2833x/common/include/DSP28x_Project.h
+Plantilla.obj: C:/ti/c2000/C2000Ware_6_00_00_00/device_support/f2833x/headers/include/DSP2833x_Device.h
+Plantilla.obj: C:/ti/c2000/C2000Ware_6_00_00_00/device_support/f2833x/headers/include/DSP2833x_Adc.h
+Plantilla.obj: C:/ti/c2000/C2000Ware_6_00_00_00/device_support/f2833x/headers/include/DSP2833x_DevEmu.h
+Plantilla.obj: C:/ti/c2000/C2000Ware_6_00_00_00/device_support/f2833x/headers/include/DSP2833x_CpuTimers.h
+Plantilla.obj: C:/ti/c2000/C2000Ware_6_00_00_00/device_support/f2833x/headers/include/DSP2833x_ECan.h
+Plantilla.obj: C:/ti/c2000/C2000Ware_6_00_00_00/device_support/f2833x/headers/include/DSP2833x_ECap.h
+Plantilla.obj: C:/ti/c2000/C2000Ware_6_00_00_00/device_support/f2833x/headers/include/DSP2833x_DMA.h
+Plantilla.obj: C:/ti/c2000/C2000Ware_6_00_00_00/device_support/f2833x/headers/include/DSP2833x_EPwm.h
+Plantilla.obj: C:/ti/c2000/C2000Ware_6_00_00_00/device_support/f2833x/headers/include/DSP2833x_EQep.h
+Plantilla.obj: C:/ti/c2000/C2000Ware_6_00_00_00/device_support/f2833x/headers/include/DSP2833x_Gpio.h
+Plantilla.obj: C:/ti/c2000/C2000Ware_6_00_00_00/device_support/f2833x/headers/include/DSP2833x_I2c.h
+Plantilla.obj: C:/ti/c2000/C2000Ware_6_00_00_00/device_support/f2833x/headers/include/DSP2833x_Mcbsp.h
+Plantilla.obj: C:/ti/c2000/C2000Ware_6_00_00_00/device_support/f2833x/headers/include/DSP2833x_PieCtrl.h
+Plantilla.obj: C:/ti/c2000/C2000Ware_6_00_00_00/device_support/f2833x/headers/include/DSP2833x_PieVect.h
+Plantilla.obj: C:/ti/c2000/C2000Ware_6_00_00_00/device_support/f2833x/headers/include/DSP2833x_Spi.h
+Plantilla.obj: C:/ti/c2000/C2000Ware_6_00_00_00/device_support/f2833x/headers/include/DSP2833x_Sci.h
+Plantilla.obj: C:/ti/c2000/C2000Ware_6_00_00_00/device_support/f2833x/headers/include/DSP2833x_SysCtrl.h
+Plantilla.obj: C:/ti/c2000/C2000Ware_6_00_00_00/device_support/f2833x/headers/include/DSP2833x_XIntrupt.h
+Plantilla.obj: C:/ti/c2000/C2000Ware_6_00_00_00/device_support/f2833x/headers/include/DSP2833x_Xintf.h
+Plantilla.obj: C:/ti/c2000/C2000Ware_6_00_00_00/device_support/f2833x/common/include/DSP2833x_Examples.h
+Plantilla.obj: C:/ti/c2000/C2000Ware_6_00_00_00/device_support/f2833x/common/include/DSP2833x_GlobalPrototypes.h
+Plantilla.obj: C:/ti/c2000/C2000Ware_6_00_00_00/device_support/f2833x/common/include/DSP2833x_EPwm_defines.h
+Plantilla.obj: C:/ti/c2000/C2000Ware_6_00_00_00/device_support/f2833x/common/include/DSP2833x_Dma_defines.h
+Plantilla.obj: C:/ti/c2000/C2000Ware_6_00_00_00/device_support/f2833x/common/include/DSP2833x_I2c_defines.h
+Plantilla.obj: C:/ti/c2000/C2000Ware_6_00_00_00/device_support/f2833x/common/include/DSP2833x_DefaultIsr.h
+
+../Plantilla.c:
+
+C:/ti/c2000/C2000Ware_6_00_00_00/device_support/f2833x/common/include/DSP28x_Project.h:
+
+C:/ti/c2000/C2000Ware_6_00_00_00/device_support/f2833x/headers/include/DSP2833x_Device.h:
+
+C:/ti/c2000/C2000Ware_6_00_00_00/device_support/f2833x/headers/include/DSP2833x_Adc.h:
+
+C:/ti/c2000/C2000Ware_6_00_00_00/device_support/f2833x/headers/include/DSP2833x_DevEmu.h:
+
+C:/ti/c2000/C2000Ware_6_00_00_00/device_support/f2833x/headers/include/DSP2833x_CpuTimers.h:
+
+C:/ti/c2000/C2000Ware_6_00_00_00/device_support/f2833x/headers/include/DSP2833x_ECan.h:
+
+C:/ti/c2000/C2000Ware_6_00_00_00/device_support/f2833x/headers/include/DSP2833x_ECap.h:
+
+C:/ti/c2000/C2000Ware_6_00_00_00/device_support/f2833x/headers/include/DSP2833x_DMA.h:
+
+C:/ti/c2000/C2000Ware_6_00_00_00/device_support/f2833x/headers/include/DSP2833x_EPwm.h:
+
+C:/ti/c2000/C2000Ware_6_00_00_00/device_support/f2833x/headers/include/DSP2833x_EQep.h:
+
+C:/ti/c2000/C2000Ware_6_00_00_00/device_support/f2833x/headers/include/DSP2833x_Gpio.h:
+
+C:/ti/c2000/C2000Ware_6_00_00_00/device_support/f2833x/headers/include/DSP2833x_I2c.h:
+
+C:/ti/c2000/C2000Ware_6_00_00_00/device_support/f2833x/headers/include/DSP2833x_Mcbsp.h:
+
+C:/ti/c2000/C2000Ware_6_00_00_00/device_support/f2833x/headers/include/DSP2833x_PieCtrl.h:
+
+C:/ti/c2000/C2000Ware_6_00_00_00/device_support/f2833x/headers/include/DSP2833x_PieVect.h:
+
+C:/ti/c2000/C2000Ware_6_00_00_00/device_support/f2833x/headers/include/DSP2833x_Spi.h:
+
+C:/ti/c2000/C2000Ware_6_00_00_00/device_support/f2833x/headers/include/DSP2833x_Sci.h:
+
+C:/ti/c2000/C2000Ware_6_00_00_00/device_support/f2833x/headers/include/DSP2833x_SysCtrl.h:
+
+C:/ti/c2000/C2000Ware_6_00_00_00/device_support/f2833x/headers/include/DSP2833x_XIntrupt.h:
+
+C:/ti/c2000/C2000Ware_6_00_00_00/device_support/f2833x/headers/include/DSP2833x_Xintf.h:
+
+C:/ti/c2000/C2000Ware_6_00_00_00/device_support/f2833x/common/include/DSP2833x_Examples.h:
+
+C:/ti/c2000/C2000Ware_6_00_00_00/device_support/f2833x/common/include/DSP2833x_GlobalPrototypes.h:
+
+C:/ti/c2000/C2000Ware_6_00_00_00/device_support/f2833x/common/include/DSP2833x_EPwm_defines.h:
+
+C:/ti/c2000/C2000Ware_6_00_00_00/device_support/f2833x/common/include/DSP2833x_Dma_defines.h:
+
+C:/ti/c2000/C2000Ware_6_00_00_00/device_support/f2833x/common/include/DSP2833x_I2c_defines.h:
+
+C:/ti/c2000/C2000Ware_6_00_00_00/device_support/f2833x/common/include/DSP2833x_DefaultIsr.h:
+
